574fde4cb55e74c44f23a0bfb250e6be1b6372d4
rishab handa <handa.rishab.93@gmail.com>
2022-11-20 20:59:14 +0530

    committing changes of c file from feature branch

--- spacex/index.js
+++ spacex/index.js
@@ -1,6 +1,7 @@
 const name = 'namer';
 console.log('new change');
 console.log('new change b');
+console.log('new change c');
 console.log('new change f');
 console.log('new change g');
 console.log('new change h');
